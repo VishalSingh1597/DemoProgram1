fe39504d5e2e0ed5873aa786ea5cd427040efa47
Vishal Singh <VishalSingh1597>
2021-03-30 17:35:22 +0530

    Added ReplaceString.cs file

---
+++ Program1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstBasicProgram
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            ReplaceString.ReadInput();
+            Console.Read();
+        }
+    }
+}
---
+++ ReplaceString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstBasicProgram
+{
+    /// <summary>
+    /// Reading input from user & Replacing username with template
+    /// </summary>
+    class ReplaceString
+    {
+        public static void ReadInput()
+        {
+            string template = "Hello <<UserName>>, How are you?";
+            Console.WriteLine("Please Enter your Name");
+            string userInput = Console.ReadLine();
+            ReplaceWord(template, userInput);
+        }
+        private static void ReplaceWord(string template, string userInput)
+        {
+            string result=template.Replace("<<userName>>", userInput);
+            Console.WriteLine("After done Replace " + result);
+        }
+    }
+}
